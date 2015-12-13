8333f51ffcedc4b60a0e81cb5aa2ab7c0f079fb0
phagenlocher <philipp.hagenlocher@tum.de>
2015-12-13 21:50:26 +0100

    Initial commit

---
+++ flask_article.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+
+import os
+from datetime import date
+
+TEMPLATER_NEWLINE = 'FLASKTEMPLATERNEWLINE'
+
+def get_script_info(script, script_folder = '/scripts'):
+	script_info = {}
+	script_info['Filename'] = script
+	data = open(os.getcwd() + '/' + script_folder + '/' + script).read()
+	data = data.split('\n')
+	try:
+		i = data.index('---End-Tags---')
+	except:
+		return None
+		
+	while '' in data:
+		data.remove('')
+		
+	tags = data[:i-1]
+	script_info['Content'] = TEMPLATER_NEWLINE.join(data[i:]).replace("\\\\", "</br>")
+	
+	for tag in tags:
+		tag_name = tag[ tag.find('{')+1 : tag.find('}') ]
+		tag_data = tag[ tag.rfind('{')+1 : tag.rfind('}') ]
+		script_info[tag_name] = tag_data
+		
+	# Check script validity
+	required_tags = ['Author','Title','Date']
+	for rt in required_tags:
+		if not rt in script_info.keys():
+			return None
+		if rt == 'Title':
+			if not script_info['Title'].startswith('_'):
+				script_info['Title'] = script_info['Title'].title()
+		if rt == 'Date':
+			d = script_info['Date'].split('/')
+			d = date(day=int(d[0]), month=int(d[1]), year=int(d[2]))
+			script_info['Date'] = d.strftime("%d. %h %Y")
+			script_info['Date_object'] = d
+		
+	return script_info
+
+def get_scripts(script_folder = '/scripts'):
+	script_infos = []
+	files = os.listdir( os.getcwd() + script_folder )
+	for script in files:
+		info = get_script_info(script, script_folder)
+		if info is not None:
+			script_infos.append(info)
+	script_infos.sort(reverse=True, key=lambda x : x['Date_object'])
+	return script_infos
+
+def generate_toc(content):
+	script = content.split(TEMPLATER_NEWLINE)
+	NUMBERED = True
+	
+	sections = []
+	section_num = 0
+	subsection_num = 1
+	for i, line in enumerate(script):
+		if line.startswith("**"):
+			top_sec = sections.pop()
+			num = float(str(section_num) + "." + str(subsection_num))
+			if line.startswith("**_"):
+				sec_title = line[3:].strip()
+			else:
+				sec_title = line[2:].strip().title()
+			top_sec.append((sec_title, i, num))
+			subsection_num += 1
+			sections.append(top_sec)
+		elif line.startswith("*"):
+			subsection_num = 1
+			section_num += 1
+			if line.startswith("*_"):
+				sec_title = line[2:].strip()
+			else:
+				sec_title = line[1:].strip().title()
+			sections.append([(sec_title, i, section_num)])
+
+	# Create html content
+	content = ""		
+	for sec_list in sections:
+		for sec in sec_list:
+			if NUMBERED:
+				sec_name = str(sec[2]) + " - " + sec[0]
+			else:
+				sec_name = sec[0]
+			if type(sec[2]) == int:
+				content += "<h2 id='" + str(sec[2]) + "'>" + sec_name + "</h2>\n<p>\n"
+			if type(sec[2]) == float:
+				content += "<h3 id='" + str(sec[2]) + "'>" + sec_name + "</h3>\n<p>\n"
+			index = sec[1] + 1
+			if index >= len(script):
+				break
+			while not script[index].startswith("*"):
+				content += script[index] + " "
+				index += 1
+				if index >= len(script):
+					break
+			content = content[:-1].replace("\\\\", "</br>")
+			content += "\n</p>\n"
+
+	# Create table of contents
+	toc = "<ul class='toc'>\n"
+	single = False
+	for sec_list in sections:
+		single = False
+		if len(sec_list) == 1:
+				single = True
+		for sec in sec_list:
+			if NUMBERED:
+				sec_name = str(sec[2]) + " - " + sec[0]
+			else:
+				sec_name = sec[0]
+			toc += "<li><a href='#" + str(sec[2]) + "'>" + sec_name + "</a></li>\n"
+			if sec_list.index(sec) == 0:
+				if not single:
+					toc += "<ul>\n"
+			if sec_list.index(sec) == len(sec_list)-1:
+				if not single:
+					toc += "</ul>\n"
+	toc += "</ul>\n"
+	return toc, content
+
+def get_article_list(format_string='{} - {}'):
+	scripts = get_scripts()
+	article_html = '';
+	for info in scripts:
+		article_html += '<a href="/' + info['Filename'] + '">' \
+		+ format_string.format(info['Date'], info['Title']) + '</a>\n'
+	return article_html
+	
---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask import render_template
+from flask_article import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	articles = get_article_list()
+	return render_template("index.html", Articles=articles)
+
+@app.route('/contact')
+def contact():
+    return 'Not done yet, dont sue'
+
+@app.route('/<article>')
+def article(article):
+	scripts = get_scripts()
+	names = []
+	for script in scripts:
+		names.append(script['Filename'])
+	if not article in names:
+		return page_not_found(None)
+		
+	script = scripts[ names.index(article) ]
+	del script['Filename']
+	script['TableOfContents'], script['Content']  = generate_toc(script['Content'])
+	command = 'render_template("article.html"'
+	for tag in script.keys():
+		command += ', ' + tag + ' = script["' + tag + '"]'
+	command += ', MathJax=False, HighlightJS=True)'
+	return eval(command)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
