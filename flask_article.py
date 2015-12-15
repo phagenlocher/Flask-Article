846bc27dafe651495f61417cc5b35e2109205122
phagenlocher <philipp.hagenlocher@tum.de>
2015-12-15 22:27:33 +0100

    Extended caching: Now ALL the information is cached which hopefully will speed up showing a list of articles.

--- flask_article.py
+++ flask_article.py
@@ -13,10 +13,10 @@ CACHE_SEPERATOR = '\n\0\n'
 class CacheHandler():
 	'''Handler for caching files
 
-	It uses 2 caches, the heap (limited) and the diskspace.
+	It uses 2 caches, the heap (limited) and the discspace.
 	The last cached scripts are the ones in heap.
 	'''
-	def __init__(self, cache_limit=1, script_folder='scripts', cache_folder='.cache', debug=True, hash_alg='sha1'):
+	def __init__(self, cache_limit=10, script_folder='scripts', cache_folder='.cache', debug=True, hash_alg='sha1'):
 		'''Constructor
 
 		Keyword arguments:
@@ -54,22 +54,30 @@ class CacheHandler():
 			shutil.rmtree(self.cache_folder)
 		os.mkdir(self.cache_folder)
 
-	def new_cache_entry(self, name, toc, content):
+	def new_cache_entry(self, script):
 		'''Creates a new cache entry in the cache dir for a file
 
 		The entry saves a hash of the script and the parsed toc and content.
 		'''
+		name = script['Filename']
 		self.report("Creating new cache entry for " + name)
-		# Writing cache entry to disk
-		disk_content = toc + CACHE_SEPERATOR + content
+
+		# Creating content
+		file_data = open(self.script_folder + '/' + name, 'rb').read()
+		h = hl.new(self.hash_alg, file_data).digest()
+		disc_content = h
+		for tag in script.keys():
+			if type(script[tag]) == str:
+				disc_content += CACHE_SEPERATOR.encode() \
+								+ tag.encode() \
+								+ b'\0' + script[tag].encode()
+
+		# Writing cache entry to disc
 		cache_path = self.cache_folder + '/' + name
 		if os.path.exists(cache_path):
 			os.remove(cache_path)
 		cache_entry = open(cache_path, 'wb')
-		file_data = open(self.script_folder + '/' + name, 'rb').read()
-		h = hl.new(self.hash_alg, file_data).digest()
-		disk_content = h + CACHE_SEPERATOR.encode() + disk_content.encode()
-		cache_entry.write(disk_content)
+		cache_entry.write(disc_content)
 
 		# Writing cache entry to heap
 		if len(self.heap_cache) < self.cache_limit:
@@ -79,7 +87,7 @@ class CacheHandler():
 		else:
 			# Delete the first entry in the cache to make room for the new one
 			del self.heap_cache[list(self.heap_cache.keys())[0]]
-		self.heap_cache[name] = (h, (toc, content))
+		self.heap_cache[name] = (h, script)
 
 	def check_cache_entry(self, name):
 		'''Checks with the hash of a cache entry if a script has changed'''
@@ -89,7 +97,7 @@ class CacheHandler():
 			# Check cache entry in heap
 			cached_h = self.heap_cache[name][0]
 		else:
-			# Check cache entry on disk
+			# Check cache entry on disc
 			cache_path = self.cache_folder + '/' + name
 			if not os.path.exists(cache_path):
 				# The cache entry does not exist
@@ -102,10 +110,17 @@ class CacheHandler():
 			self.report('Loaded {} from heap cache'.format(name))
 			return self.heap_cache[name][1]
 		else:
+			script = {}
 			cache_entry = open(self.cache_folder + '/' + name, 'rb').read()
 			cache_entry = cache_entry.split(CACHE_SEPERATOR.encode())
-			self.report('Loaded {} from disk cache'.format(name))
-			return cache_entry[1].decode(), cache_entry[2].decode()
+			for item in cache_entry:
+				item = item.split(b'\0')
+				tag = item[0].decode()
+				value = item[1].decode()
+				script[tag] = value
+			self.__parse_date__(script)
+			self.report('Loaded {} from disc cache'.format(name))
+			return script
 
 class ScriptLoader():
 	'''Loader for the scripts'''
@@ -144,71 +159,73 @@ class ScriptLoader():
 		Keyword arguments:
 		script_name -- script name
 		'''
-		script = self.__get_script_info__(script_name)
-		if script == None:
-			return None
-		
-		# Checking cache entry
-		if not self.cache_handler.check_cache_entry(script_name):
-			# Parsing file and creating new cache entry
-			script['TableOfContents'], script['Content'] = self.__parse_content__(script['Content'])
-			self.cache_handler.new_cache_entry(script_name, script['TableOfContents'], script['Content'])
-		else:
-			# Loading parsed contents
-			script['TableOfContents'], script['Content'] = self.cache_handler.get_cached_content(script_name)
+		return self.__get_script_info__(script_name)
 
-		return script
-
-	def __get_script_info__(self, script):
+	def __get_script_info__(self, script_name):
 		'''Open a script and parse its content 
 		If the script is faulty, None is returned
 
 		Keyword arguments:
-		script -- script name
+		script_name -- script name
 		'''
 		script_info = {}
-		script_info['Filename'] = script
+		script_info['Filename'] = script_name
 
 		# The file couldn't be found
-		if not script in os.listdir(os.getcwd() + '/' + self.script_folder):
+		script_path = os.getcwd() + '/' + self.script_folder
+		if not script_name in os.listdir(script_path):
 			return None
 
-
-		data = open(os.getcwd() + '/' + self.script_folder + '/' + script).read()
-		data = data.split('\n')
-		try:
-			i = data.index('---End-Tags---')
-		except:
-			return None
-			
-		while '' in data:
-			data.remove('')
-			
-		tags = data[:i-1]
-		script_info['Content'] = TEMPLATE_NEWLINE.join(data[i:]).replace("\\\\", "</br>")
-		
-		for tag in tags:
-			tag_name = tag[ tag.find('{')+1 : tag.find('}') ]
-			tag_data = tag[ tag.rfind('{')+1 : tag.rfind('}') ]
-			script_info[tag_name] = tag_data
-			
-		# Check script validity
-		required_tags = ['Author','Title','Date']
-		for rt in required_tags:
-			if not rt in script_info.keys():
+		# Checking cache entry
+		if not self.cache_handler.check_cache_entry(script_name):
+			# Parsing file and creating new cache entry
+			data = open(script_path + '/' + script_name).read()
+			data = data.split('\n')
+			try:
+				i = data.index('---End-Tags---')
+			except:
 				return None
-			# Make the title titlecased
-			if rt == 'Title':
-				if not script_info['Title'].startswith('_'):
-					script_info['Title'] = script_info['Title'].title()
-			# Create a date-object (later used for sorting)
-			if rt == 'Date':
-				d = script_info['Date'].split('/')
-				d = date(day=int(d[0]), month=int(d[1]), year=int(d[2]))
-				script_info['Date'] = d.strftime("%d. %h %Y")
-				script_info['Date_object'] = d
-			
+				
+			while '' in data:
+				data.remove('')
+				
+			tags = data[:i-1]
+			script_info['Content'] = TEMPLATE_NEWLINE.join(data[i:]).replace("\\\\", "</br>")
+
+			for tag in tags:
+				tag_name = tag[ tag.find('{')+1 : tag.find('}') ]
+				tag_data = tag[ tag.rfind('{')+1 : tag.rfind('}') ]
+				script_info[tag_name] = tag_data
+				
+			# Check script validity
+			required_tags = ['Author','Title','Date']
+			for rt in required_tags:
+				if not rt in script_info.keys():
+					return None
+				# Make the title titlecased
+				if rt == 'Title':
+					if not script_info['Title'].startswith('_'):
+						script_info['Title'] = script_info['Title'].title()
+				# Create a date-object (later used for sorting)
+				if rt == 'Date':
+					self.__parse_date__(script_info)
+
+			# Final formatting
+			self.__parse_content__(script_info)
+
+			# Caching
+			self.cache_handler.new_cache_entry(script_info)
+		else:
+			# Loading parsed contents
+			script_info = self.cache_handler.get_cached_content(script_name)	
+
 		return script_info
+
+	def __parse_date__(self, script_info):
+		d = script_info['Date'].split('/')
+		d = date(day=int(d[0]), month=int(d[1]), year=int(d[2]))
+		script_info['Date'] = d.strftime("%d. %h %Y")
+		script_info['Date_object'] = d
 
 	def __get_scripts__(self, sort=True, key='Date_object'):
 		'''Get all the scripts from the script directory and optionally sort them
@@ -227,14 +244,14 @@ class ScriptLoader():
 			script_infos.sort(reverse=True, key=lambda x : x[key])
 		return script_infos
 
-	def __parse_content__(self, content, numbered=True):
+	def __parse_content__(self, script_info, numbered=True):
 		'''Generates a table of contents and modifies an articles content
 
 		Keyword arguments:
 		content -- the content to parse
 		numbered -- if the sections should be numbered (default True)
 		'''
-		script = content.split(TEMPLATE_NEWLINE)
+		script = script_info['Content'].split(TEMPLATE_NEWLINE)
 		
 		sections = []
 		section_num = 0
@@ -302,7 +319,8 @@ class ScriptLoader():
 					if not single:
 						toc += "</ul>\n"
 		toc += "</ul>"
-		return toc, content
+		script_info['TableOfContents'] = toc
+		script_info['Content'] = content
 
 	def get_article_list(self, format_string='{} - {}', sort=True, key='Date_object'):
 		'''Get a list of html-links to all the articles
