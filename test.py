54f64394ebe720b62053cfa68a6f0310bbff5fb4
phagenlocher <philipp.hagenlocher@tum.de>
2015-12-18 23:58:34 +0100

    Added new test script to test caching aswell as a new test folder full of big scripts to test performance

---
+++ test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+from time import time
+from statistics import mean
+from flask_article import ScriptLoader
+
+def print_result(result):
+	print('| Min (in seconds)\t| Mean (in seconds)\t| Max (in seconds)\t|')
+	print('|:---------------------:|:---------------------:|:---------------------:|')
+	print('|',min(result),'\t|',mean(result),'\t|',max(result),'\t|')
+
+def test_script_loader(sl, iterations):
+	print('Timing get_article_list...')
+	result = []
+	for i in range(iterations):
+		t1 = time()
+		sl.get_article_list()
+		t2 = time()
+		result.append(t2-t1)
+	print_result(result)
+
+	print('Timing get_single_script...')
+	result = []
+	for i in range(iterations):
+		t1 = time()
+		sl.get_single_script('test1')
+		t2 = time()
+		result.append(t2-t1)
+	print_result(result)
+
+##
+iterations = 100
+##
+
+print("Testing with caching...")
+sl = ScriptLoader(script_folder='test', caching=True, debug=False)
+test_script_loader(sl, iterations)
+
+print("\n\nTesting without caching...")
+sl = ScriptLoader(script_folder='test', caching=False, debug=False)
+test_script_loader(sl, iterations)
